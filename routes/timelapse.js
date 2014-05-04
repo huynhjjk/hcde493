611614a8c0c7d2a785287ccd89740a5fb694613e
unknown <huynhjjk@yahoo.com>
2014-05-04 16:07:29 -0700

    mihir

--- routes/timelapse.js
+++ routes/timelapse.js
@@ -167,8 +167,8 @@ exports.startCamera = function(req, res) {
 		shell.exec(str,function(code, output) {
 		    console.log('avconv reached output ' + output + ' code ' + code);
 		    // shell.cd('~/hcde493');
-			var scp = "scp -r jmzhwng@vergil.u.washington.edu:/nfs/bronfs/uwfs/dw00/d96/jmzhwng/Images";
-			console.log("this is scp " + scp);
+			// var scp = "scp -r " + pathname + " jmzhwng@vergil.u.washington.edu:/nfs/bronfs/uwfs/dw00/d96/jmzhwng/Images";
+			// console.log("this is scp " + scp);
 			// shell.exec(scp,function(code, output) {
 			//     console.log('scp reached output ' + output + ' code ' + code);
 			 	res.json(options, 200);
