cf15821361e747e04f6047fb9ce77f3ba311bc0f
unknown <huynhjjk@yahoo.com>
2014-05-05 11:34:46 -0700

    convert Images button

--- routes/timelapse.js
+++ routes/timelapse.js
@@ -119,33 +119,52 @@ exports.setCamera = function(req, res) {
 	console.log('SET CAMERA - ' + JSON.stringify(settings));
 }
 
+exports.convertImages = function(req, res) {
+	var dirname = req.params.folderName;
+	var pathname = process.cwd() + "public/images/" + dirname;
+	shell.cd(pathname);
+	shell.exec("avconv -r 1 -i image%04d.jpeg -r 1 -vcodec libx264 -crf 20 -g 15 -vf scale=1280:720 timelapse.mp4",function(code, output) {
+	    console.log('gst-launch reached. output: ' + output + ' code: ' + code);
+	    shell.rm('*jpeg');
+	    shell.cd('../../..');
+		var scp = "scp -r " + pathname + " jmzhwng@vergil.u.washington.edu:/nfs/bronfs/uwfs/dw00/d96/jmzhwng/Images";
+		console.log("this is scp " + scp);
+		shell.exec(scp,function(code, output) {
+		    console.log('scp reached. output: ' + output + ' code: ' + code);
+		 	res.json(settings, 200);
+		});
+	});
+}
+
 exports.startCamera = function(req, res) {
 	settings = req.body;
 
 	var d = new Date();
 	var dirname = "pics_" + d.toUTCString().replace(/\s+/g, '').replace(/:/g, '_');
-	var pathname = "public/images/" + dirname;
+	var pathname = process.cwd() + "public/images/" + dirname;
 	var output = "image%d.jpg";
 	shell.mkdir('-p', pathname);
 
 	var timelapse = (settings.intervalMinutes * 60000) + (settings.intervalSeconds * 1000);
 	var timeout = (settings.durationHours * 3600000) + (settings.durationMinutes * 60000) + (settings.durationSeconds * 1000);
 
+
+	// "gst-launch-1.0 multifilesrc location=image%04d.jpeg index=1 caps='image/jpeg,framerate=10/1' ! jpegdec ! omxh264enc ! avimux ! filesink location=timelapse.avi"
+
  	shell.cd(pathname);
 	shell.exec("raspistill -o image%04d.jpeg -tl" + " " + timelapse + " " + "-t" + " " + timeout + " -w 1920 -h 1080",function(code, output) {
 	    console.log('raspistill reached. output: ' + output + ' code: ' + code);
-// "gst-launch-1.0 multifilesrc location=image%04d.jpeg index=1 caps='image/jpeg,framerate=10/1' ! jpegdec ! omxh264enc ! avimux ! filesink location=timelapse.avi"
-		shell.exec("avconv -r 1 -i image%04d.jpeg -r 1 -vcodec libx264 -crf 20 -g 15 -vf scale=1280:720 timelapse.mp4",function(code, output) {
-		    console.log('gst-launch reached. output: ' + output + ' code: ' + code);
-		    shell.rm('*jpeg');
+		// shell.exec("avconv -r 1 -i image%04d.jpeg -r 1 -vcodec libx264 -crf 20 -g 15 -vf scale=1280:720 timelapse.mp4",function(code, output) {
+		//     console.log('gst-launch reached. output: ' + output + ' code: ' + code);
+		//     shell.rm('*jpeg');
 		    shell.cd('../../..');
-			var scp = "scp -r " + pathname + " jmzhwng@vergil.u.washington.edu:/nfs/bronfs/uwfs/dw00/d96/jmzhwng/Images";
-			console.log("this is scp " + scp);
-			shell.exec(scp,function(code, output) {
-			    console.log('scp reached. output: ' + output + ' code: ' + code);
+		// 	var scp = "scp -r " + pathname + " jmzhwng@vergil.u.washington.edu:/nfs/bronfs/uwfs/dw00/d96/jmzhwng/Images";
+		// 	console.log("this is scp " + scp);
+		// 	shell.exec(scp,function(code, output) {
+		// 	    console.log('scp reached. output: ' + output + ' code: ' + code);
 			 	res.json(settings, 200);
-			});
-		});
+		// 	});
+		// });
 	});
 
 	// var options = {
