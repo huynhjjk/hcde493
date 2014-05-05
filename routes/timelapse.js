28419cdad668303071fe0f85c83a80ebe97504cc
unknown <huynhjjk@yahoo.com>
2014-05-04 17:18:15 -0700

    brian stuff

--- routes/timelapse.js
+++ routes/timelapse.js
@@ -161,7 +161,7 @@ exports.startCamera = function(req, res) {
 	camera.on("exit", function( timestamp ){
 	  console.log("timelapse child process has exited");
 	 	shell.cd(pathname);
-		var str = "avconv -r 10 -i image%d.jpg -r 10 -vcodec libx264 -crf 20 -g 15 timelapse.mp4"
+		var str = "avconv -r " + req.body.settings.fps + " -i image%d.jpg -r " + req.body.settings.fps + " -vcodec libx264 -crf 20 -g 15 timelapse.mp4"
 		shell.exec(str,function(code, output) {
 		    console.log('avconv reached output ' + output + ' code ' + code);
 		    shell.rm('*jpg');
