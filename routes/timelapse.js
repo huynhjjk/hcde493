40ec83dbbb26bc6658a112567ce369e8a835dd48
unknown <huynhjjk@yahoo.com>
2014-05-05 14:02:16 -0700

    checking out convert button

--- routes/timelapse.js
+++ routes/timelapse.js
@@ -159,7 +159,7 @@ exports.startCamera = function(req, res) {
 			//     if (code === 0) {
 				    // console.log('gst-launch reached. output: ' + output + ' code: ' + code);
 				    // shell.rm('*jpeg');
-				    // shell.cd('../../..');
+				    shell.cd('../../..');
 					// var scp = "scp -r " + pathname + " jmzhwng@vergil.u.washington.edu:/nfs/bronfs/uwfs/dw00/d96/jmzhwng/Images";
 					// console.log("this is scp " + scp);
 					// shell.exec(scp,function(code, output) {
